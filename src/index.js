8c56d828736c11d69e6971d7e2906285b3ef2320
belizario87 <leonardobelizario88@gmail.com>
2024-03-04 12:14:35 -0300

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const server = require('../src/server/Server')
+const server = require('../src/server/Server');
 
-const PORT = 3131
+const PORT = 3131;
 
 server.listen(PORT, () => {
-    console.log(`O servidor está rodando na porta ${PORT}`);
-})
+	console.log(`O servidor está rodando na porta ${PORT}`);
+});
