da3d92b442f1ef4f304e918a09796c654b3e6c5c
belizario87 <leonardobelizario88@gmail.com>
2024-04-02 07:49:19 -0300

    Test router post

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,4 +6,7 @@ router.get("/", (req, res) => {
     res.send("funcionando");
 });
 
+router.post('/', (req, res) => {
+    return res.status(statusCode.OK).json(req.body);
+})
 module.exports = router;
